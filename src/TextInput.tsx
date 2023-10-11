b1df465ddda12e183692f872c2a5ec6bc5a02159
sethclim <sethclim@gmail.com>
2023-10-11 12:06:41 -0400

    feat: added text input

---
+++ src/TextInput.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { VStack } from './primatives/Stack';
+import { Text, TextInput, type TextStyle, type ViewStyle } from 'react-native';
+
+type TextInputProps = {
+  containerStyle: ViewStyle;
+  errorStyle: TextStyle;
+  textInputStyle: TextStyle;
+  text: string;
+  textChanged: () => {};
+  errorMessage: string;
+};
+
+export const TextInputField = (props: TextInputProps) => {
+  return (
+    <VStack style={props.containerStyle}>
+      <Text style={props.errorStyle}>{props.errorMessage}</Text>
+      <TextInput
+        value={props.text}
+        onChangeText={() => props.textChanged()}
+        style={props.textInputStyle}
+      />
+    </VStack>
+  );
+};
