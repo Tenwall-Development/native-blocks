f63df4cd81b588dd8932ea27dbd8285386a8e494
sethclim <sethclim@gmail.com>
2024-04-06 19:28:51 -0400

    Started calender and progress bar

---
+++ Calander.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { HStack, VStack } from './primatives/Stack';
+import { Text, type TextStyle, type ViewStyle } from 'react-native';
+import { withStyle } from './hoc/WithStyle';
+
+type DayProps = {
+    day : String
+    boxStyle?: ViewStyle
+    textStyle: TextStyle
+}
+
+const Day = (props: DayProps) =>{
+    return(
+        <VStack flexCross={false} flexMain={false} width={20} height={30} style={props.boxStyle}>
+            <Text style={props.textStyle}>{props.day}</Text>
+        </VStack>
+    )
+}
+
+type CalanderRowProps = {
+    daysInRow : Number[]
+    textStyle: TextStyle
+}
+
+const CalanderRow = (props : CalanderRowProps) =>{
+    return(
+        <HStack flexMain={false} flexCross={false}  >
+            {
+                props.daysInRow.map( (num, idx) =>{
+                    return <Day key={idx} day={`${num}`} textStyle={props.textStyle} boxStyle={{backgroundColor: 'red'}} />
+                })
+            }
+        </HStack>
+    )
+}
+
+type CalanderProps = {
+    textStyle?: TextStyle
+}
+
+export const Calander = (props : CalanderProps) => {
+    useEffect(()=>{
+        console.log(JSON.stringify(props))
+    })
+  return (
+    <VStack style={{backgroundColor: "#000000"}} align='flex-start'>
+        <CalanderRow daysInRow={[1,2,3,4,5,6,7]} textStyle={props.textStyle!} />
+        <CalanderRow daysInRow={[1,2,3,4,5,6,7]} textStyle={props.textStyle!} />
+        <CalanderRow daysInRow={[1,2,3,4,5,6,7]} textStyle={props.textStyle!} />
+        <CalanderRow daysInRow={[1,2,3,4,5,6,7]} textStyle={props.textStyle!} />
+        <CalanderRow daysInRow={[1,2,3]} textStyle={props.textStyle!} />
+    </VStack>
+  )
+};
+
+const makeStyle = (theme: any) => {
+    return {
+        textStyle:{
+            color: "#ffffff"
+        }
+    };
+  };
+  
+export const StyledCalander = withStyle<CalanderProps>(
+    Calander,
+    makeStyle
+);
---
+++ ProgressBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { withStyle } from './hoc/WithStyle';
+import { Text, type DimensionValue, type TextStyle, type ViewStyle } from 'react-native';
+import { HStack } from '../../src/components/primatives/Stack';
+
+export type ProgressBarProps = {
+    backgroundStyle?: ViewStyle;
+    barStyle?: ViewStyle;
+    textStyle?: TextStyle;
+    height?: DimensionValue;
+    progress: Number
+}
+
+export const ProgessBar = (props: ProgressBarProps) => {
+  return (
+    <HStack style={props.backgroundStyle} height={props.height} flexMain={props.height == undefined} justifyContent='flex-start'>
+        <HStack style={props.barStyle} width={`${props.progress}%` as DimensionValue} flexMain={false} justifyContent='flex-start'>
+            <Text style={props.textStyle}>{`${props.progress}%`}</Text>
+        </HStack>
+    </HStack>
+  )
+};
+
+const makeStyle = (theme: any) => {
+    return {
+      backgroundStyle: {
+        backgroundColor: theme.primary,
+      },
+      barStyle: {
+        backgroundColor: theme.secondary,
+      },
+      textStyle:{
+        color: theme.primary,
+      }
+    };
+  };
+  
+  export const StyledProgessBar = withStyle<ProgressBarProps>(
+    ProgessBar,
+    makeStyle
+  );
+  
