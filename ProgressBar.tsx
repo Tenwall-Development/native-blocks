6daa9fad8889b092321e8c8c5c0edbff020f0b65
sethclim <56656904+sethclim@users.noreply.github.com>
2025-03-09 00:49:06 -0500

    updated progress bar import

--- ProgressBar.tsx
+++ ProgressBar.tsx
@@ -6,7 +6,7 @@ import {
   type TextStyle,
   type ViewStyle,
 } from 'react-native';
-import { HStack } from '../../src/components/primatives/Stack';
+import { HStack } from './primatives/Stack';
 
 export type ProgressBarProps = {
   backgroundStyle?: ViewStyle;
